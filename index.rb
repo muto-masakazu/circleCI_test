5c65380d561b7177f274cf927298ba0611fd68cc
muto <masa0509mutou@yahoo.co.jp>
2019-08-11 00:12:43 +0900

    dev commit

--- index.rb
+++ index.rb
@@ -2,4 +2,7 @@ module Calc
     def self.sum(a, b)
         return a + b
     end
+    def self.minus(a, b)
+        return a
+    end
 end
--- test.rb
+++ test.rb
@@ -5,4 +5,7 @@ class CalculationTest < Minitest::Test
   def test_sum
     assert_equal(Calc.sum(1, 2), 3)
   end
+  def test_minus
+    assert_equal(Calc.minus(1, 2), -1)
+  end
 end
